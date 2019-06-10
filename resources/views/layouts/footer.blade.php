fd0e3f6a717c3508b8bdc317a54e0f0fbf8207e4
Felix Avelar <felixavco@gmail.com>
2019-06-10 00:07:38 -0600

    fist commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="mt-4">
+    <div class="container">
+        <h6 class="text-center text-secondary lead">
+            {{ config('app.name', 'Laravel') }}
+            &reg;
+            {{ date('Y') }}
+        </h6>
+    </div>
+</footer>
